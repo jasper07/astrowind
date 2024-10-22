1f7492218e532ea7292a3c02a814ca9c684c9b00
John Patterson <John.Patterson@secondphase.com.au>
2024-10-22 20:15:41 +1100

    upgrade astro

--- astro.config.ts
+++ astro.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
           'currency-exchange',
           'voice-presentation',
           'business-contact',
-          'database',
+          'database',      
           'collaboration',
           'todo-list',
         ],
